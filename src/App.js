a714670ba001f3bca828bb9872ed4077bb020bc2
yassine errakkas <yassine.errakkas11@gmail.com>
2024-06-02 19:12:11 +0100

    created the base app routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import CreateVote from "./pages/CreateVote";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="">App</div>;
+  return (
+    <div className="bg-slate-100">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-vote" element={<CreateVote />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
